b29984ec484b89bd01b5933a3e49f5903428af82
Parth0124 <abhangparth@gmail.com>
2025-09-17 16:13:53 +0530

    added frontend to main

---
+++ frontend/src/App.tsx
@@ -0,0 +1,45 @@
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Info from "./components/Info";
+import Navbar from "./components/Navbar";
+import IngredientUploadPage from "./pages/Ingrident";
+
+// Layout wrapper to handle conditional Footer
+const Layout = () => {
+  const location = useLocation();
+
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        {/* Home Page */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <Info />
+            </>
+          }
+        />
+
+        {/* Ingredient Upload Page */}
+        <Route path="/upload" element={<IngredientUploadPage onNavigate={() => {}} />} />
+      </Routes>
+
+      {/* Hide footer only on /upload */}
+      {location.pathname !== "/upload" && <Footer />}
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Layout />
+    </Router>
+  );
+};
+
+export default App;
